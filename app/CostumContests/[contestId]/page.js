1efce8dab89d0f51d72a34b9d35baf31590f045a
Madhava Reddy <madhava2807@gmail.com>
2025-02-27 12:02:26 +0530

    share button in contest

--- app/CostumContests/[contestId]/page.js
+++ app/CostumContests/[contestId]/page.js
@@ -1,13 +1,27 @@
+"use client"
 import Header from '@/components/Header'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import { useToast } from "@/hooks/use-toast"
+import { useParams } from 'next/navigation'
+import ContestHeader from '@/components/ContestHeader/ContestHeader'
 const page = () => {
+  const[contestId,setContestId]=useState();
+  const params=useParams();
+  console.log(params)
+ useEffect(()=>{
+  if(params)
+
+    { 
+      setContestId(params?.contestId);
+
+     console.log(contestId)}
+ },[params.contestId])
   return (
     <div className='flex '>
-        <Header/>
-        <div>
-            HI
-        </div>
+        <ContestHeader/>
+        <div className='mt-[100px]'>
+             Hi
+          </div>
     </div>
   )
 }
